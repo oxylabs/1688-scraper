3c6f2564f6884621635b10978f3203619a0f8bbd
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:15:26 +0200

    Adding code examples/1688_golang.go

---
+++ code examples/1688_golang.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	const Username = "YOUR_USERNAME"
+	const Password = "YOUR_PASSWORD"
+
+	payload := map[string]string{
+		"source": "universal_ecommerce",
+		"url": "https://fuzhuang.1688.com/nanzhuang?spm=a260k.dacugeneral.home2019category.2.663335e4bwa222",
+	}
+
+	jsonValue, _ := json.Marshal(payload)
+
+	client := &http.Client{}
+	request, _ := http.NewRequest("POST",
+		"https://realtime.oxylabs.io/v1/queries",
+		bytes.NewBuffer(jsonValue),
+	)
+
+	request.SetBasicAuth(Username, Password)
+	response, _ := client.Do(request)
+
+	responseText, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(responseText))
+}
