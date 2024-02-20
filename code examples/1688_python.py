299bf36588d69b6dff34eea14c008c3131df2ad8
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:15:25 +0200

    Adding code examples/1688_python.py

---
+++ code examples/1688_python.py
@@ -0,0 +1,20 @@
+import requests
+from pprint import pprint
+
+# Structure payload.
+payload = {
+    'source': 'universal_ecommerce',
+    'url': 'https://fuzhuang.1688.com/nanzhuang?spm=a260k.dacugeneral.home2019category.2.663335e4bwa222'
+}
+
+# Get response.
+response = requests.request(
+    'POST',
+    'https://realtime.oxylabs.io/v1/queries',
+    auth=('user', 'pass1'),
+    json=payload,
+)
+
+# Instead of response with job status and results url, this will return the
+# JSON response with the result.
+pprint(response.json())
