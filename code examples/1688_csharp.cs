bfca899de7b11f43d8ad7dd80990da25428111f1
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:15:23 +0200

    Adding code examples/1688_csharp.cs

---
+++ code examples/1688_csharp.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OxyApi
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string Username = "YOUR_USERNAME";
+            const string Password = "YOUR_PASSWORD";
+
+            var parameters = new Dictionary<string, string>()
+            {
+                { "source", "universal_ecommerce" },
+                { "url", "https://fuzhuang.1688.com/nanzhuang?spm=a260k.dacugeneral.home2019category.2.663335e4bwa222" },
+            };
+
+
+            var client = new HttpClient();
+
+            Uri baseUri = new Uri("https://realtime.oxylabs.io");
+            client.BaseAddress = baseUri;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/v1/queries");
+            requestMessage.Content = JsonContent.Create(parameters);
+
+            var authenticationString = $"{Username}:{Password}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authenticationString));
+            requestMessage.Headers.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+
+            var response = await client.SendAsync(requestMessage);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(contents);
+        }
+    }
+}
