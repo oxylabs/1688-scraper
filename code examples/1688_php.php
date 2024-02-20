277a201aef516d8028ee175648aa950caa8c9904
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:15:19 +0200

    Adding code examples/1688_php.php

---
+++ code examples/1688_php.php
@@ -0,0 +1,27 @@
+<?php
+
+$params = [
+    'source' => 'universal_ecommerce',
+    'url' => 'https://fuzhuang.1688.com/nanzhuang?spm=a260k.dacugeneral.home2019category.2.663335e4bwa222',
+];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://realtime.oxylabs.io/v1/queries");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_USERPWD, "user" . ":" . "pass1");
+
+$headers = array();
+$headers[] = "Content-Type: application/json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
