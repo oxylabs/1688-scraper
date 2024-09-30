a1601c055622af6ceca0c55c4c195b482c125702
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-09-30 16:35:36 +0300

    Update 1688_nodejs.js

--- code examples/1688_nodejs.js
+++ code examples/1688_nodejs.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const username = 'YOUR_USERNAME';
 const password = 'YOUR_PASSWORD';
 const body = {
-  'source': 'universal_ecommerce',
+  'source': 'universal',
   'url': 'https://fuzhuang.1688.com/nanzhuang?spm=a260k.dacugeneral.home2019category.2.663335e4bwa222',
 };
 const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
