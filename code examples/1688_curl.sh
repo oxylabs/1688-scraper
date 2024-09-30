279adaf7477954395051557d02b21a07bce29999
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-09-30 16:35:05 +0300

    Update 1688_curl.sh

--- code examples/1688_curl.sh
+++ code examples/1688_curl.sh
@@ -1,4 +1,4 @@
 curl --user user:pass \
 'https://realtime.oxylabs.io/v1/queries' \
 -H "Content-Type: application/json" \
--d '{"source": "universal_ecommerce", "url": "https://fuzhuang.1688.com/nanzhuang?spm=a260k.dacugeneral.home2019category.2.663335e4bwa222"}'
+-d '{"source": "universal", "url": "https://fuzhuang.1688.com/nanzhuang?spm=a260k.dacugeneral.home2019category.2.663335e4bwa222"}'
